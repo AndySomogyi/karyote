d7810d10aa7947ff7928301d657c9eba91d166db
andy.somogyi <andy.somogyi@gmail.com>
2013-04-16 16:17:07 -0400

    updates

--- verify_stochiometry.m
+++ verify_stochiometry.m
@@ -8,21 +8,38 @@ function [  ] = verify_stochiometry( s, z )
     
     s = reshape(s, [n_species*n_comp, n_reactions]);
     
-    for i=1:n_reactions
-        assert(sum(s(:,i)) == 0, sprintf('mass balance not conserved in reaction %i', i));
-    end
-    disp('mass balance appears OK');
+    %for i=1:n_reactions
+    %    assert(sum(s(:,i)) == 0, sprintf('mass balance not conserved in reaction %i', i));
+    %end
+    %disp('mass balance appears OK');
     
     
     
     assert(n_species == length(z), 'valences must be the same length as the number of species');
     
-    z = kron(z, ones(1,n_comp));
+    z = kron(ones(1,n_comp), z);
+    r = zeros(n_species*n_comp, 1);
     for i=1:n_reactions
-        assert(z * s(:,i) == 0, sprintf('charge balance not conserved in reaction %i', i));
+        % reactants
+        ind = find(s(:,i) < 0);
+        r(:) = 0;
+        r(ind) = abs(s(ind,i));
+        
+        lhs = z * r;
+        
+        % products
+        ind = find(s(:,i) > 0);
+        r(:) = 0;
+        r(ind) = abs(s(ind,i));
+        
+        rhs = z * r;
+        
+        assert(lhs == rhs, ...
+            sprintf('charge balance not conserved in reaction %i, consumed: %i, produced: %i', ...
+            i, lhs, rhs));
     end
-    disp('charge balance appears OK');
     
+
     disp('stoichiometry matrix appears OK');
     
 end
