b91231b421f4a2e72dafb7b78d765ed47636ec9c
Yeiji Im <yeijim@dione.(none)>
2013-05-09 11:17:03 -0400

    update

---
+++ test_mito1.m
@@ -0,0 +1,66 @@
+% set the global constants
+global F R T
+F= 9.64853399e4; R=8.3144621; T=300;
+
+% 
+% 
+
+n_comp = 4;
+n_species = 1;
+n_reactions = 0;
+n_trans_reactions = 0;
+
+% concentration, n_species, n_comp
+% each column is a compartment, each row in the column is a species.
+c0 = [0 1]  * 5.2e-6;
+
+  
+% stoichiometry matrix n_species, n_comp, n_reactions
+si = zeros(n_species, n_comp, n_reactions);
+st = zeros(n_species, n_comp, n_trans_reactions);
+
+% forward and backward rate constants, 
+% n_reactions, 2.
+ki = zeros(n_reactions,2);
+kt = zeros(n_trans_reactions,2);
+
+
+% valences, each species has same valence everywhere, n_species.
+z = [1];
+
+% initial compartment voltage
+v = [0, 1];
+
+cap = abs(eye(n_comp,n_comp) - 1);
+
+a = abs(eye(n_comp,n_comp) - 1);
+l = abs(eye(n_comp,n_comp) - 1);
+o = ones(n_comp, 1);
+o(1) = inf;
+
+r = inf(n_comp,n_comp);
+
+% membrane permeability
+% test with membrane partially permeable to all species.
+h = ones(n_species, n_comp, n_comp) * 1;
+
+fun = odefun(cap, a, l, h, z, o, si, ki, st, kt, r);
+
+state = karyote_pack(c0,v);
+
+[t,y] = ode15s(fun, [0 2], state);
+
+c = y(:,1:(n_species*n_comp));
+v = y(:,(n_species*n_comp)+1:end);
+
+
+disp(c(1,:));
+disp(c(end,:));
+    
+subplot(2,1,1);
+plot(t,c);
+
+subplot(2,1,2);
+plot(t,v);
+
+
