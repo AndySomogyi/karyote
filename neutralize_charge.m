ba94026639a395d941a4e53acb6c2f8c6c60c469
andy.somogyi <andy.somogyi@gmail.com>
2013-05-21 12:27:59 -0400

    neutralize charge should be working correctly now

--- neutralize_charge.m
+++ neutralize_charge.m
@@ -1,26 +1,57 @@
 function [ c ] = neutralize_charge( c, z )
 % concentration, n_species, n_comp
 % z: 1,n_species;
-    zp = zeros(size(z));
-    lp = logical(z > 0);
-    zp(lp) = z(lp);
-    zm = zeros(size(z));
-    lm = logical(z < 0);
-    zm(lm) = z(lm);
 
-    for i = 1:size(c,2)
-        q = z * c(:,i);
-        if q < 0
-            scale = -q/(zp * c(:,i));
-            c(lp,i) = (1+scale) * c(lp,i);
-            warning('charge in compartment %i is %f, scaling positive charge by %f%', i, q, scale);
-        elseif q > 0
-            scale = -q/(zm * c(:,i));
-            c(lm,i) = (1+scale) * c(lm,i);
-            warning('charge in compartment %i is %f, scaling negative charge by %f%', i, q, scale);
+
+    n_comp = size(c,2);
+    zp = zeros(size(z));
+    zn = zeros(size(z));
+        
+    for i = 1:n_comp
+
+        cc=c(:,i);
+        ip1 = logical(cc > 0 & z' > 0);
+        ip2 = logical(cc < 0 & z' < 0);
+        ip = ip1 | ip2;
+        in1 = logical(cc > 0 & z' < 0);
+        in2 = logical(cc < 0 & z' > 0);
+        in = in1 | in2;
+        
+        zp(:) = 0;
+        zp(ip1) =  z(ip1);
+        zp(ip2) =  z(ip2);
+        
+        zn(:) = 0;
+        zn(in1) = -z(in1);
+        zn(in2) = -z(in2);
+        
+        qp =  zp * cc;
+        qn =  zn * cc;
+        
+        if qp > qn
+            s = qp / qn;
+            c(in,i) = s * cc(in);
+            fprintf('excess positive chage in comp %i, increasing negative charge by %3.2f%%\n', ...
+                i, (s-1)*100);
+        elseif qn > qp
+            s = qn / qp;
+            c(ip,i) = s * cc(ip);
+            fprintf('excess negative chage in comp %i, increasing positive charge by %3.2f%%\n', ...
+                i, (s-1)*100);
         else
-            fprintf('charge in compartment %i is 0\n', i);
-        end 
+            fprintf('charge is neutral in comp %i\n', i);
+        end
+        
+        % the total charge caried by this flux, calculated with the
+        % real valence vector, this had better be zero, or very near.
+        q = z * c(:,i);
+        
+        fprintf('total charge in comp %i after neutralization: %d\n', i, q);
+        
+        if abs(q) > 1e-15
+            error('death and horror!, unable to neutralize change %d in comp %i\n', ...
+                q, i);
+        end
     end
 end
 
